df5613cf8db2cfdb4b5b83687ba7e6652de70b12
balobaidi <59498657+balobaidi@users.noreply.github.com>
2023-11-23 02:03:34 -0500

    Update BoundaryDestroyer.cs

--- Assets/Scripts/BoundaryDestroyer.cs
+++ Assets/Scripts/BoundaryDestroyer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//may be replaced with raycaster
 public class BoundaryDestroyer : MonoBehaviour
 {
     private void OnTriggerExit2D(Collider2D other)
