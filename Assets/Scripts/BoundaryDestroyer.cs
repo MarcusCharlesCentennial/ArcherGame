273f1678cc4e51c700563a3aa5d992dc195c6718
Shemal Grey <shemalgrey@gmail.com>
2023-11-24 15:21:56 -0500

    Game Over Screen
    When the player falls out of bounds loads the game over screen

--- Assets/Scripts/BoundaryDestroyer.cs
+++ Assets/Scripts/BoundaryDestroyer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //may be replaced with raycaster
 public class BoundaryDestroyer : MonoBehaviour
@@ -8,5 +9,10 @@ public class BoundaryDestroyer : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other)
     {
         Destroy(other.gameObject);
+        GameOver();
+    }
+    private void GameOver()
+    {
+        SceneManager.LoadScene("GameOverScreen");
     }
 }
