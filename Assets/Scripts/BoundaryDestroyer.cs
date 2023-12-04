7cf50e74984e2dcbebf6f6e56859ae4c1dc7f113
Marcus Charles <Sucrammoses@gmail.com>
2023-12-04 16:11:28 -0500

    Death boundary
    Falling out of world kills player

--- Assets/Scripts/BoundaryDestroyer.cs
+++ Assets/Scripts/BoundaryDestroyer.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 //may be replaced with raycaster
 public class BoundaryDestroyer : MonoBehaviour
 {
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(other.gameObject);
-
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject); // Destroy the player
+            GameOver();
+        }
+    }
+    private void GameOver()
+    {
+        SceneManager.LoadScene("GameOverScene");
     }
 }
